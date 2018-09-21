939a366d246bbd47731a9b8755e5a3986b2285bf
Mahamudun Hassan <mahamudun.totaltvs@gmail.com>
2018-09-21 19:58:55 +0600

    delete some function

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -4,24 +4,6 @@
      class TopicsController extends AppController
      {
      	
-     	public function add()
-     	{
-
-     	}
-
-     	public function edit()
-     	{
-
-     	}
-
-     	public function update()
-     	{
-     		
-     	}
-
-     	public function delete()
-     	{
-
-     	}
+     	
      	
      }
