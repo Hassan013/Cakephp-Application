dfdfd7c36802c069345a44358fc336cedc7de35a
Mahamudun Hassan <mahamudun.totaltvs@gmail.com>
2018-09-21 20:03:09 +0600

    add topic controller function

--- app/Controller/TopicsController.php
+++ app/Controller/TopicsController.php
@@ -4,6 +4,28 @@
      class TopicsController extends AppController
      {
      	
-     	
+	     	public function addTopic()
+	     	{
+	     		
+
+	     	}
+
+	     	public function editTopic()
+	     	{
+	     		
+
+	     	}
+
+	     	public function updateTopic()
+	     	{
+	     		
+
+	     	}
+
+	     	public function deleteTopic()
+	     	{
+	     		
+
+	     	}
      	
      }
